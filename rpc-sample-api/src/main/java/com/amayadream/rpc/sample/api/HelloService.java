28743674673a67bfd667596b2f98336a2eec4d13
Amayadream <524806599@qq.com>
2016-07-14 22:11:25 +0800

    添加项目依赖

---
+++ rpc-sample-api/src/main/java/com/amayadream/rpc/sample/api/HelloService.java
@@ -0,0 +1,11 @@
+package com.amayadream.rpc.sample.api;
+
+/**
+ * @author :  Amayadream
+ * @date :  2016.07.14 22:07
+ */
+public interface HelloService {
+
+    String hello(String name);
+
+}
---
+++ rpc-sample-server/src/main/java/com/amayadream/rpc/sample/server/HelloServiceImpl.java
@@ -0,0 +1,9 @@
+package com.amayadream.rpc.sample.server;
+
+/**
+ * @author :  Amayadream
+ * @date :  2016.07.14 22:09
+ */
+public class HelloServiceImpl {
+
+}
